7e6bd5a795fb0a31f20b9185b6f99c2c8e8bbf17
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-10-12 23:00:49 +0300

    variables

---
+++ Degiskenler/2-Değişkenler/script.js
@@ -0,0 +1,14 @@
+// Değişkenler
+
+var age;
+console.log(age); // undefined
+
+age = 20;
+console.log(age); // 20
+
+var fullname = "Recep Kaya";
+console.log(fullname); // Sadık Turan
+
+const mail = "abc.gmail.com";
+// const sabit değer içeriği değişmez
+console.log(mail);
