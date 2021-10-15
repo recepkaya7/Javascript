7ec5e66de37065947de7056e101b21856f483208
Recep KAYA <58885111+recepkaya7@users.noreply.github.com>
2021-10-16 00:08:19 +0300

    Math library

---
+++ sayıları yuvarlama/10-numbers/script.js
@@ -0,0 +1,18 @@
+let val;
+
+val = Math.PI;
+val = Math.round(2.4); //yakın sayıya yuvarlar
+val = Math.round(2.7);
+val = Math.ceil(2.4); // yukarıya yuvarlar
+val = Math.ceil(2.6);
+val = Math.floor(2.4); // aşağıya yuvarlar
+val = Math.floor(2.7);
+val = Math.sqrt(64);
+val = Math.pow(2, 4);
+val = Math.abs(-100); //mutlak değer
+val = Math.min(1, 2, 3, 2, 4, 9);
+val = Math.max(4, 5, 6, 4, 98, 7);
+val = Math.floor(Math.random() * 100 + 1);
+
+console.log(val);
+console.log(typeof val);
